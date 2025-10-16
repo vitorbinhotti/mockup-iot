c9275e0f64b98b366efa4ccf79ca1faf5f80822d
duducode7 <eduardo_oliveira37@estudante.sesisenai.org.br>
2025-10-16 09:47:54 -0300

    conectando ao broker

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,4 +1,13 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
+const String BrokerURL = "test.mosquitto.org";     //endereço do broker público
+const int BrokerPort = 1883;                       //porta do broker púlbico
+const String BrokerUser = "";                      //usuário do servidor
+const String BrokerPass = "";                      //senha do usuário
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
@@ -12,6 +21,17 @@ void setup() {
     delay(200);
   }
   Serial.println("\nConectado com Sucesso!");
+
+  Serial.println("Conectando ao Broker...");
+  mqtt.setServer(BrokerURL.c_str(),BrokerPort);
+  String BoardID = "Trem";
+  BoardID += String(random(0xffff), HEX);
+  mqtt.connect(BoardID.c_str() , BrokerUser.c_str() , BrokerPass.c_str());
+  while(!mqtt.connected()){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado ao Broker!");
 }
 
 void loop() {
