62f7c5870465b10e254d3e7284a62355a95076b7
mateusselhorst <mateus_selhorst@estudante.sesisenai.org.br>
2025-10-16 09:47:21 -0300

    Conectanbdo

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,4 +1,14 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
+const String BrokerURL = "test.mosquitto.org";
+const int BrokerPort = 1883;
+
+const String BrokerUser = "";
+const String BrokerPass = "";
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
@@ -13,6 +23,18 @@ void setup() {
     delay(200);
   }
   Serial.print("\nConectado com sucesso!");
+
+  Serial.print("\nConectando ao Broker...");
+  mqtt.setServer(BrokerURL.c_str(),BrokerPort);
+  String BoardID = "S3";
+  BoardID += String (random(0xffff), HEX);
+  mqtt.connect(BoardID.c_str() , BrokerUser.c_str() , BrokerPass.c_str());
+
+  while(!mqtt.connected()){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado ao Broker!");
 }
 
 void loop() {
