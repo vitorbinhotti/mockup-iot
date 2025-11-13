aed2d3743bac89dbbb7aef842b71a2b9ef26ba28
duducode7 <eduardo_oliveira37@estudante.sesisenai.org.br>
2025-11-13 10:04:23 -0300

    Leitura do sensor de luminosidade com LED

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,51 +1,31 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include "env.h"
 
-WiFiClient client;
-PubSubClient mqtt(client);
-
-
+#define LDR_PIN 34
+#define LED_PIN 19  
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Conectando ao WiFi");  //apresanta essa msg na tela
-  WiFi.begin(WIFI_SSID, WIFI_PASS);                //tenta conectar na rede
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");  //mostra ".........."
-    delay(200);
-  }
-  Serial.println("\nConectado com Sucesso!");
-
-  Serial.println("Conectando ao Broker...");
-  mqtt.setServer(BROKER_URL,BROKER_PORT);
-  String BoardID = "Trem";
-  BoardID += String(random(0xffff), HEX);
-  mqtt.connect(BoardID.c_str() , BROKER_USER , BROKER_PASS);
-  while(!mqtt.connected()){
-    Serial.print(".");
-    delay(200);
-  }
-  mqtt.subscribe(TOPIC_ILUM);
-  mqtt.setCallback(callback);
-  Serial.println("\nConectado ao Broker!");
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(LDR_PIN, INPUT);
 }
 
 void loop() {
-  mqtt.publish(TOPIC_ILUM , "Acender");
-  mqtt.loop();
+  int valorLDR = analogRead(LDR_PIN);
+  Serial.print("Valor do LDR: ");
+  Serial.println(valorLDR);
+
+  int limite = 3000; 
+
+  if (valorLDR > limite) {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("ambiente escuro");
+  } else {
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("ambiente claro");
+  }
+
   delay(1000);
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  String msg = "";
-  for (int i = 0 < length, i++) {
-    msg += (char)payload[i];
-  }
-
-  if (topic == TOPIC_ILUM && ,msg == "Acender") {
-    digitalWrite(2, HIGH);
-  } else if (topic == TOPIC_ILUM && msg == "Apagar") {
-    digitalWrite (2, LOW);
-  }
 }
