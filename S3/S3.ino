843d71b321a013868f84c853f685ad3689b3ad73
mateusselhorst <mateus_selhorst@estudante.sesisenai.org.br>
2025-11-19 10:12:26 -0300

    Arrumado sensor

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,52 +1,65 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
-#include <ESP32Servo.h>
 #include "env.h"
+#include <ESP32Servo.h>
 
 WiFiClientSecure client;
 PubSubClient mqtt(client);
 
-Servo meuServo;
+
+
 
 const byte SERVO_PIN = 18;
-const byte TRIGGER_PIN = 26;
-const byte ECHO_PIN = 25;
+Servo Servo1;
 
-unsigned long lastMsg = 0;
-char msg[50];
-bool objetoProximoAnterior = false; 
+const byte SERVO_PIN2 = 19;
+Servo Servo2;
 
-void conectaWiFi() {
-  Serial.println("Conectando ao WiFi...");
+const byte TRIGGER_PIN = 25;
+const byte ECHO_PIN = 26;
+
+
+
+void setup() {
+  Serial.begin(115200);
+
+
+  Servo1.attach(SERVO_PIN);
+  Servo2.attach(SERVO_PIN2);
+
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+
+  client.setInsecure();
+  Serial.println("Conectando ao WiFi");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    delay(500);
+    delay(200);
   }
-  Serial.println("\nWiFi conectado!");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
-}
-
-void conectaMQTT() {
+  Serial.print("\nConectado com Sucesso!");
+  Serial.println("Conectando ao Broker...");
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
+  mqtt.setCallback(callback);
+  String BoardID = "S2";
+  BoardID += String(random(0xffff, HEX));
+  mqtt.connect(BoardID.c_str(), BROKER_USER, BROKER_PASS);
   while (!mqtt.connected()) {
-    Serial.print("Conectando ao broker MQTT...");
-    String clientId = "ESP32Client-";
-    clientId += String(random(0xffff), HEX);
-
-    if (mqtt.connect(clientId.c_str(), BROKER_USER, BROKER_PASS)) {
-      Serial.println("Conectado!");
-      mqtt.subscribe(TOPIC_ILUM);
-    } else {
-      Serial.print("Falha (rc=");
-      Serial.print(mqtt.state());
-      Serial.println("), tentando novamente em 5s");
-      delay(5000);
-    }
+    Serial.println(".");
+    delay(200);
   }
+  
+  Serial.println("\nConectado ao Broker!");
+  mqtt.subscribe(TOPIC_ILUM);
+  mqtt.subscribe(TOPIC_PRESENCA1);
+  mqtt.subscribe(TOPIC_PRESENCA2);
+  mqtt.subscribe(TOPIC_PRESENCA3);
 }
+
+
 
 long lerDistancia() {
   digitalWrite(TRIGGER_PIN, LOW);
@@ -56,9 +69,30 @@ long lerDistancia() {
   digitalWrite(TRIGGER_PIN, LOW);
 
   long duracao = pulseIn(ECHO_PIN, HIGH);
-  long distancia = duracao * 0.034 / 2;
+  long distancia = duracao * 349.24 / 2 / 10000;
 
   return distancia;
+}
+
+
+
+void loop() {
+
+  mqtt.loop();
+
+  long distancia = lerDistancia();
+
+  Serial.print("Distância: ");
+  Serial.print(distancia);
+  Serial.println(" cm");
+
+  if (distancia < 10) {
+    Serial.println("Objeto próximo!");
+    mqtt.publish(TOPIC_DISTANCIA, "Objeto próximo!");
+  }
+
+  delay(1000);
+
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -67,63 +101,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
     msg += (char)payload[i];
   }
 
-  Serial.print("Mensagem recebida em ");
-  Serial.print(topic);
-  Serial.print(": ");
-  Serial.println(msg);
+Serial.print("TOPICO: "); Serial.println(topic);
+  Serial.print("MSG: "); Serial.println(msg);
 
-  if (String(topic) == TOPIC_ILUM) {
-    if (msg == "Acender") {
-      for (int pos = 0; pos <= 180; pos++) {
-        meuServo.write(pos);
-        delay(10);
-      }
-    } else if (msg == "Apagar") {
-      for (int pos = 180; pos >= 0; pos--) {
-        meuServo.write(pos);
-        delay(10);
-      }
+  if (strcmp(topic, TOPIC_ILUM) == 0) {
+    if (msg == "ligar") digitalWrite(2, HIGH);
+    if (msg == "desligar") digitalWrite(2, LOW);
+  }else if (strcmp(topic, TOPIC_PRESENCA1) == 0) {
+    if (msg == "servo1") {
+      Servo1.write(0);
+      Serial.println("Servo1 → posição 1 (0°)");
     }
-  }
-}
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  meuServo.attach(SERVO_PIN);
-
-  client.setInsecure();
-  conectaWiFi();
-
-  mqtt.setServer(BROKER_URL, BROKER_PORT);
-  mqtt.setCallback(callback);
-  conectaMQTT();
-}
-
-void loop() {
-  if (!mqtt.connected()) {
-    conectaMQTT();
-  }
-  mqtt.loop();
-
-  unsigned long agora = millis();
-  if (agora - lastMsg > 2000) {
-    lastMsg = agora;
-
-    long distancia = lerDistancia();
-    Serial.print("Distância: ");
-    Serial.print(distancia);
-    Serial.println(" cm");
-
-    if (distancia > 0 && distancia < 10) {
-      if (!objetoProximoAnterior) { 
-        mqtt.publish(TOPIC_ILUM, "Objeto proximo");
-        Serial.println("Objeto próximo! Enviado ao MQTT.");
-        objetoProximoAnterior = true;
-      }
-    } else {
-      objetoProximoAnterior = false;
+  }else if (strcmp(topic, TOPIC_PRESENCA2) == 0) {
+    if (msg == "servo2") {
+      Servo2.write(0);
+      Serial.println("Servo2 → posição 1 (0°)");
+    }
+  }else if (strcmp(topic, TOPIC_PRESENCA3) == 0) {
+    if (msg == "abrir") {
+      Servo1.write(90);
+      Servo2.write(90);
+      Serial.println("Servo1 e Servo2 → posição 2 (90°)");
     }
   }
 }
